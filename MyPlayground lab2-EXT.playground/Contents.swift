fff0918d8eab1b0ad825cfe3960218e15316f77f
Mohammed Abdullah <mona@MacBook-Pro-alkhas-b-mohammed.local>
2021-10-13 11:27:16 +0300

    done lab_EXT

---
+++ MyPlayground lab2-EXT.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+extension Double {
+    
+    var cubed:Double {
+        
+        self * self * self
+        
+    }
+}
+
+print(20.5.cubed)
+
+
+
+
+//Create an extension for Double
+//
+//Create computed variable cubed
+//It should compute the third power of the number
+//Use the newly created extension to find the cube of 20.5
+//Print the result
